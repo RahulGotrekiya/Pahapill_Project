41995ce4fa33d3920a8f59971336f6c611dc01db
RahulGotrekiya <rahulgotrekiya2603@gmail.com>
2024-01-07 19:32:14 +0530

    Homepage Completed

--- Pahapill/assets/js/custom.js
+++
@@ -1,8 +0,0 @@
-let hamburger = document.querySelector(".hamburger");
-let nav = document.querySelector(".header-menu");
-let bod = document.querySelector(".container");
-
-hamburger.addEventListener("click", function () {
-  hamburger.classList.toggle("isactive");
-  nav.classList.toggle("active");
-});
